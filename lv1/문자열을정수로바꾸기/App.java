7bf04ad3ed76059797f5fa36fbf5773650a3c18c
devsince2021 <dev.ryu2021@gmail.com>
2024-01-09 22:51:39 +0900

    문자열을 정수로 바꾸기

---
+++ lv1/문자열을정수로바꾸기/App.java
@@ -0,0 +1,9 @@
+package lv1.문자열을정수로바꾸기;
+
+
+public class App {
+  private static String input = "-1234";
+  public static void main(String[] args) {
+    System.out.println(new Solution().solution(input));
+  }
+}
