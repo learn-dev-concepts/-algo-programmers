8ce903a522f5714c057c90c35bdcdbcb606a945b
devsince2021 <dev.ryu2021@gmail.com>
2024-01-14 14:47:27 +0900

    두정수사이의합

---
+++ lv1/두정수사이의합/App.java
@@ -0,0 +1,10 @@
+package lv1.두정수사이의합;
+
+
+public class App {
+  private static int input1 = 3;
+  private static int input2 = 5;
+  public static void main(String[] args) {
+    System.out.println(new Solution().solution(input1, input2));
+  }
+}
