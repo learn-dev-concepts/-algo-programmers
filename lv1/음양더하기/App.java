db01e761497e87f75a69f1837a15185f2ab84ada
devsince2021 <dev.ryu2021@gmail.com>
2024-01-14 15:05:56 +0900

    음양더하기

---
+++ lv1/음양더하기/App.java
@@ -0,0 +1,11 @@
+package lv1.음양더하기;
+
+
+public class App {
+  private static int[] input1 = {4, 7, 12};
+  private static boolean[] input2 = {true, false, true};
+
+  public static void main(String[] args) {
+    System.out.println(new Solution().solution(input1, input2));
+  }
+}
