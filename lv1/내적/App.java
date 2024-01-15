a308207bf72060364caa11055347cecbb1d3f8bb
devsince2021 <dev.ryu2021@gmail.com>
2024-01-16 02:44:18 +0900

    내적

---
+++ lv1/내적/App.java
@@ -0,0 +1,11 @@
+package lv1.내적;
+
+
+public class App {
+  private static int[] input1 = {1, 2, 3, 4};
+  private static int[] input2 = {-3, -1, 0, 2};
+
+  public static void main(String[] args) {
+    System.out.println(new Solution().solution(input1, input2));
+  }
+}
