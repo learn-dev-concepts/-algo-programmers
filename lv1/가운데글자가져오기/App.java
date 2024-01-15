d28bde1bf006d51a531d4b3f4a9347a45b8b90de
devsince2021 <dev.ryu2021@gmail.com>
2024-01-16 03:05:08 +0900

    가운데 글자 가져오기

---
+++ lv1/가운데글자가져오기/App.java
@@ -0,0 +1,10 @@
+package lv1.가운데글자가져오기;
+
+
+public class App {
+  private static String input = "abcde";
+
+  public static void main(String[] args) {
+    System.out.println(new Solution().solution(input));
+  }
+}
