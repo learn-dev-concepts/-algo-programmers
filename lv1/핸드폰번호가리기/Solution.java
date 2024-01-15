01053d0277e051f644fff08a842d4f1302c44f75
devsince2021 <dev.ryu2021@gmail.com>
2024-01-15 18:50:10 +0900

    핸드폰 번호 가리기

---
+++ lv1/핸드폰번호가리기/Solution.java
@@ -0,0 +1,19 @@
+package lv1.핸드폰번호가리기;
+
+
+
+class Solution {
+  public String solution(String phone_number) {
+    StringBuilder answer = new StringBuilder();
+    String[] arr = phone_number.split("");
+
+    for (int i = 0; i < arr.length; i++) {
+      boolean shouldShow = i >= arr.length - 4;
+      String el = shouldShow ? arr[i] : "*";
+      answer.append(el);
+    }
+
+    return answer.toString();
+
+  }
+}
