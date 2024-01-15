c7a0245e18970ccca5d9440e8868637890841bcc
devsince2021 <dev.ryu2021@gmail.com>
2024-01-15 17:57:50 +0900

    김서방 찾기

---
+++ lv1/서울에서김서방찾기/App.java
@@ -0,0 +1,9 @@
+package lv1.서울에서김서방찾기;
+
+
+public class App {
+  private static String[] input = {"jane", "kim"};
+  public static void main(String[] args) {
+    System.out.println(new Solution().solution(input));
+  }
+}
