ee620b404aece2fbff8f11777579729962ca7821
devsince2021 <dev.ryu2021@gmail.com>
2024-01-16 13:53:52 +0900

    문자열 내림차순으로 배치하기

--- template/Solution.java
+++ template/Solution.java
@@ -5,10 +5,6 @@ import java.util.stream.IntStream;
 
 class Solution {
   public int solution(int n) {
-    OptionalInt sum = IntStream.rangeClosed(1, n).reduce((acc, cur) ->
-            n % cur == 0 ? acc + cur : acc
-    );
-
-    return sum.orElse(0);
+    return 0;
   }
 }
