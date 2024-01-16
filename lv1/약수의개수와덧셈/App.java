054448a1dd02134f512c39ff01d40e3cdbf1e7b3
devsince2021 <dev.ryu2021@gmail.com>
2024-01-16 12:14:04 +0900

    약수의 개수와 덧셈

---
+++ lv1/약수의개수와덧셈/App.java
@@ -0,0 +1,10 @@
+package lv1.약수의개수와덧셈;
+
+
+public class App {
+  private static int input1 = 13;
+  private static int input2 = 17;
+  public static void main(String[] args) {
+    System.out.println(new Solution().solution(input1, input2));
+  }
+}
