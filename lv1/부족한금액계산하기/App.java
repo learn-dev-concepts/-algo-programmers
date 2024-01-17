56215e611e88ed48704d4a790f6698370d09c3d7
devsince2021 <dev.ryu2021@gmail.com>
2024-01-17 15:16:16 +0900

    부족한 금액 계산하기

---
+++ lv1/부족한금액계산하기/App.java
@@ -0,0 +1,12 @@
+package lv1.부족한금액계산하기;
+
+
+public class App {
+  private static int input1 = 3;
+  private static int input2 = 20;
+  private static int input3 = 4;
+
+  public static void main(String[] args) {
+    System.out.println(new Solution().solution(input1, input2, input3));
+  }
+}
