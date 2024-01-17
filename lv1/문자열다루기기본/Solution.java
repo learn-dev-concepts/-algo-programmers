92048331179cde8a05edbc16191f70e2aaa31ff2
devsince2021 <dev.ryu2021@gmail.com>
2024-01-17 15:20:56 +0900

    문자열 다루기 기본

---
+++ lv1/문자열다루기기본/Solution.java
@@ -0,0 +1,20 @@
+package lv1.문자열다루기기본;
+
+class Solution {
+  public boolean solution(String s) {
+    boolean answer = true;
+    int len = s.length();
+
+    if (len != 4 && len != 6) {
+      answer = false;
+    }
+
+    try {
+      Integer.parseInt(s);
+    } catch (Exception e) {
+      answer = false;
+    }
+
+    return answer;
+  }
+}
