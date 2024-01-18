356867874fa444eedfe290eefd541640e46007fd
devsince2021 <dev.ryu2021@gmail.com>
2024-01-19 02:44:21 +0900

    같은 숫자는 싫어

---
+++ lv1/같은숫자는싫어/App.java
@@ -0,0 +1,10 @@
+package lv1.같은숫자는싫어;
+
+
+public class App {
+  private static int input[] = {1, 1, 3, 3, 0, 1, 1};
+
+  public static void main(String[] args) {
+    System.out.println(new Solution().solution(input));
+  }
+}
